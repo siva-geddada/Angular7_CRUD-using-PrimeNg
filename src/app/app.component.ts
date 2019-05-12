96c9dd6e4937943dcad677df405711343a804545
siveswararao geddada <sgeddada@corp.miraclesoft.com>
2019-05-12 22:11:16 +0530

    Add user inital commi

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular7-CRUD';
+  display = false;
+
+  showDialog() {
+      this.display = true;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,62 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+
+//** PrimeNg Modules**/
+import {InputTextModule} from 'primeng/inputtext';
+import {AccordionModule} from 'primeng/accordion';
+import {CheckboxModule} from 'primeng/checkbox';
+import {DropdownModule} from 'primeng/dropdown';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {RadioButtonModule} from 'primeng/radiobutton';
+import {CardModule} from 'primeng/card';
+import {ToastModule} from 'primeng/toast';
+import {MessagesModule} from 'primeng/messages';
+import {MessageModule} from 'primeng/message';
+import {ButtonModule} from 'primeng/button';
+import {PanelModule } from 'primeng/primeng';
+import {SidebarModule} from 'primeng/sidebar';
+import {ToolbarModule} from 'primeng/toolbar';
+import {PasswordModule} from 'primeng/password';
+import {InputSwitchModule} from 'primeng/inputswitch';
+import {CalendarModule} from 'primeng/calendar';
+import {InputMaskModule} from 'primeng/inputmask';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import {SplitButtonModule} from 'primeng/splitbutton';
+import {DialogModule} from 'primeng/dialog';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    InputTextModule,
+    AccordionModule,
+    CheckboxModule,
+    FormsModule,
+    DropdownModule,
+    BrowserAnimationsModule,
+    RadioButtonModule,
+    CardModule,
+    ToastModule,
+    MessagesModule,
+    MessageModule,
+    ButtonModule,
+    PanelModule,
+    SidebarModule,
+    ToolbarModule,
+    CalendarModule,
+    PasswordModule,
+    InputSwitchModule,
+    InputMaskModule,
+    SplitButtonModule,
+    DialogModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
